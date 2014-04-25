5a1516ee1e52bc535c4ab468012ffb6a13ce5e14
Alejandro Eguren <aeguren@bu.edu>
2014-04-24 21:47:37 -0400

    Test
    Lets see

---
+++ Testing/button.h
@@ -0,0 +1,19 @@
+//OUR PROJECT
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include <QToolButton>
+
+//! [0]
+class Button : public QToolButton
+{
+    Q_OBJECT
+
+public:
+    explicit Button(const QString &text, QWidget *parent = 0);
+
+    QSize sizeHint() const;
+};
+//! [0]
+
+#endif
---
+++ Testing/main.cpp
@@ -0,0 +1,11 @@
+//OUR PROJECT
+#include <QApplication>
+#include "calculator.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    Calculator calc;
+    calc.show();
+    return app.exec();
+}
