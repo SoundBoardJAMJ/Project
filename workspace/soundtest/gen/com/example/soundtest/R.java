f57b85ea621c570995362e18d7a82b352ff1cdef
jiantan1 <jiant@bu.edu>
2014-04-28 21:57:06 -0400

    workspace commit

--- workspace/soundtest/gen/com/example/soundtest/R.java
+++ workspace/soundtest/gen/com/example/soundtest/R.java
@@ -995,18 +995,25 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f060016;
         public static final int action_menu_presenter=0x7f060017;
         public static final int action_mode_close_button=0x7f060024;
-        public static final int action_settings=0x7f060046;
+        public static final int action_settings=0x7f06004d;
         public static final int activity_chooser_view_content=0x7f060025;
         public static final int always=0x7f06000b;
         public static final int beginning=0x7f060011;
         public static final int button1=0x7f06003e;
-        public static final int button2=0x7f06003f;
-        public static final int button3=0x7f060040;
-        public static final int button4=0x7f060041;
-        public static final int button5=0x7f060042;
-        public static final int button6=0x7f060043;
-        public static final int button7=0x7f060044;
-        public static final int button8=0x7f060045;
+        public static final int button10=0x7f060047;
+        public static final int button11=0x7f060048;
+        public static final int button12=0x7f06004b;
+        public static final int button13=0x7f060049;
+        public static final int button14=0x7f06004a;
+        public static final int button15=0x7f06004c;
+        public static final int button2=0x7f060040;
+        public static final int button3=0x7f06003f;
+        public static final int button4=0x7f060043;
+        public static final int button5=0x7f060045;
+        public static final int button6=0x7f060044;
+        public static final int button7=0x7f060042;
+        public static final int button8=0x7f060041;
+        public static final int button9=0x7f060046;
         public static final int checkbox=0x7f06002d;
         public static final int collapseActionView=0x7f06000d;
         public static final int container=0x7f06003c;
@@ -1113,7 +1120,20 @@ containing a value of this type.
         public static final int main=0x7f0d0000;
     }
     public static final class raw {
-        public static final int button3=0x7f050000;
+        public static final int burp=0x7f050000;
+        public static final int button3=0x7f050001;
+        public static final int chimp=0x7f050002;
+        public static final int clap=0x7f050003;
+        public static final int drumroll=0x7f050004;
+        public static final int drumroll_dramatic=0x7f050005;
+        public static final int funky_drummer=0x7f050006;
+        public static final int gun_shoot=0x7f050007;
+        public static final int handroll_wav=0x7f050008;
+        public static final int kendrum=0x7f050009;
+        public static final int laser=0x7f05000a;
+        public static final int snare=0x7f05000b;
+        public static final int sonar=0x7f05000c;
+        public static final int tom=0x7f05000d;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
--- workspace/soundtest/src/com/example/soundtest/MainActivity.java
+++ workspace/soundtest/src/com/example/soundtest/MainActivity.java
@@ -20,47 +20,19 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		
-		//View.OnClickListener myhandler1;
-	//	Button mbutton;
-		//mbutton = (Button) findViewById(R.id.button1);
-		//mbutton.setOnClickListener(myhandler1);
-
-		/*mbutton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v){
-				//media.start();
-			}
-		});*/
-			
-			
+				
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
 	}
 	
-	/*8View.OnClickListener myhandler1 = new View.OnClickListener() {
-	    public void onClick(View v) {
-	      // it was the 1st button
-	    }
-	  };
-	  */
-
-	
-	/*final MediaPlayer media3 = MediaPlayer.create(this, R.raw.button3);
-	final MediaPlayer media4 = MediaPlayer.create(this, R.raw.button3);
-	final MediaPlayer media5 = MediaPlayer.create(this, R.raw.button3);
-	final MediaPlayer media6 = MediaPlayer.create(this, R.raw.button3);
-	final MediaPlayer media7 = MediaPlayer.create(this, R.raw.button3);
-	final MediaPlayer media8 = MediaPlayer.create(this, R.raw.button3);*/
 	  public void ButtonOnClick(View v) {
 		  AudioManager audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
-		  audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 5, 0);
+		  setVolumeControlStream(AudioManager.STREAM_MUSIC);
 		    switch (v.getId()) {
 		      case R.id.button1:
-		    	  MediaPlayer media1 = MediaPlayer.create(this, R.raw.button3);
+		    	  MediaPlayer media1 = MediaPlayer.create(this, R.raw.handroll_wav);
 		    	  media1.start();
 		    	  media1.setOnCompletionListener(new OnCompletionListener() {
 		    		    public void onCompletion(MediaPlayer media1) {
@@ -70,7 +42,7 @@ public class MainActivity extends ActionBarActivity {
 		    		});
 		        break;
 		      case R.id.button2:
-		    	  MediaPlayer media2 = MediaPlayer.create(this, R.raw.button3);
+		    	  MediaPlayer media2 = MediaPlayer.create(this, R.raw.gun_shoot);
 		    	  media2.start();
 		    	  media2.setOnCompletionListener(new OnCompletionListener() {
 		    		    public void onCompletion(MediaPlayer media2) {
@@ -79,24 +51,136 @@ public class MainActivity extends ActionBarActivity {
 		    		    };
 		    		});
 		        break;
-		     /* case R.id.button3:
+		      case R.id.button3:
+		    	  MediaPlayer media3 = MediaPlayer.create(this, R.raw.burp);
 		    	  media3.start();
+		    	  media3.setOnCompletionListener(new OnCompletionListener() {
+		    		    public void onCompletion(MediaPlayer media3) {
+		    		        media3.release();
+
+		    		    };
+		    		});
 		        break;
 		      case R.id.button4:
+		    	  MediaPlayer media4 = MediaPlayer.create(this, R.raw.laser);
 		    	  media4.start();
+		    	  media4.setOnCompletionListener(new OnCompletionListener() {
+		    		    public void onCompletion(MediaPlayer media4) {
+		    		        media4.release();
+
+		    		    };
+		    		});
 		        break;
 		      case R.id.button5:
+		    	  MediaPlayer media5 = MediaPlayer.create(this, R.raw.snare);
 		    	  media5.start();
+		    	  media5.setOnCompletionListener(new OnCompletionListener() {
+		    		    public void onCompletion(MediaPlayer media5) {
+		    		        media5.release();
+
+		    		    };
+		    		});
 		        break;
 		      case R.id.button6:
+		    	  MediaPlayer media6 = MediaPlayer.create(this, R.raw.chimp);
 		    	  media6.start();
+		    	  media6.setOnCompletionListener(new OnCompletionListener() {
+		    		    public void onCompletion(MediaPlayer media6) {
+		    		        media6.release();
+
+		    		    };
+		    		});
 		        break;
 		      case R.id.button7:
+		    	  MediaPlayer media7 = MediaPlayer.create(this, R.raw.button3);
 		    	  media7.start();
+		    	  media7.setOnCompletionListener(new OnCompletionListener() {
+		    		    public void onCompletion(MediaPlayer media7) {
+		    		        media7.release();
+
+		    		    };
+		    		});
 		        break;
 		      case R.id.button8:
+		    	  MediaPlayer media8 = MediaPlayer.create(this, R.raw.sonar);
 		    	  media8.start();
-		        break;*/
+		    	  media8.setOnCompletionListener(new OnCompletionListener() {
+		    		    public void onCompletion(MediaPlayer media8) {
+		    		        media8.release();
+
+		    		    };
+		    		});
+		        break;
+		      case R.id.button9:
+		    	  MediaPlayer media9 = MediaPlayer.create(this, R.raw.tom);
+		    	  media9.start();
+		    	  media9.setOnCompletionListener(new OnCompletionListener() {
+		    		    public void onCompletion(MediaPlayer media9) {
+		    		        media9.release();
+
+		    		    };
+		    		});
+		        break;
+		      case R.id.button10:
+		    	  MediaPlayer media10 = MediaPlayer.create(this, R.raw.sonar);
+		    	  media10.start();
+		    	  media10.setOnCompletionListener(new OnCompletionListener() {
+		    		    public void onCompletion(MediaPlayer media10) {
+		    		        media10.release();
+
+		    		    };
+		    		});
+		        break;
+		      case R.id.button11:
+		    	  MediaPlayer media11 = MediaPlayer.create(this, R.raw.sonar);
+		    	  media11.start();
+		    	  media11.setOnCompletionListener(new OnCompletionListener() {
+		    		    public void onCompletion(MediaPlayer media11) {
+		    		        media11.release();
+
+		    		    };
+		    		});
+		        break;
+		      case R.id.button12:
+		    	  MediaPlayer media12 = MediaPlayer.create(this, R.raw.sonar);
+		    	  media12.start();
+		    	  media12.setOnCompletionListener(new OnCompletionListener() {
+		    		    public void onCompletion(MediaPlayer media12) {
+		    		        media12.release();
+
+		    		    };
+		    		});
+		        break;
+		      case R.id.button13:
+		    	  MediaPlayer media13 = MediaPlayer.create(this, R.raw.sonar);
+		    	  media13.start();
+		    	  media13.setOnCompletionListener(new OnCompletionListener() {
+		    		    public void onCompletion(MediaPlayer media13) {
+		    		        media13.release();
+
+		    		    };
+		    		});
+		        break;
+		      case R.id.button14:
+		    	  MediaPlayer media14 = MediaPlayer.create(this, R.raw.sonar);
+		    	  media14.start();
+		    	  media14.setOnCompletionListener(new OnCompletionListener() {
+		    		    public void onCompletion(MediaPlayer media14) {
+		    		        media14.release();
+
+		    		    };
+		    		});
+		        break;
+		      case R.id.button15:
+		    	  MediaPlayer media15 = MediaPlayer.create(this, R.raw.sonar);
+		    	  media15.start();
+		    	  media15.setOnCompletionListener(new OnCompletionListener() {
+		    		    public void onCompletion(MediaPlayer media15) {
+		    		        media15.release();
+
+		    		    };
+		    		});
+		        break;
 		      }
 		  
 		    
